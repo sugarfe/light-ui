12502e93cf9305cc8d6e835291370b000d5dd5f6
于鲸浩 <yu_jinghao@126.com>
2019-06-09 21:36:03 +0800

    工程配置相关

--- build/webpack/rules.js
+++ build/webpack/rules.js
@@ -1,53 +1,64 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 function resolve(dir) {
-    return path.join(__dirname, '../../', dir)
+	return path.join(__dirname, '../../', dir);
 }
 
-let sassLoader = [MiniCssExtractPlugin.loader, 'css-loader?minimize', 'postcss-loader', 'sass-loader', {
-    loader: 'sass-resources-loader',
-    options: {
-        resources: [
-            path.resolve(__dirname, '../../src/style/mixin/border1px.scss'),
-            path.resolve(__dirname, '../../src/style/mixin/flex.scss'),
-            path.resolve(__dirname, '../../src/style/variables/color.scss'),
-            path.resolve(__dirname, '../../src/style/variables/size.scss')
-        ]
-    }
-}]
+let sassLoader = [
+	MiniCssExtractPlugin.loader,
+	'css-loader?minimize',
+	'postcss-loader',
+	'sass-loader',
+	{
+		loader: 'sass-resources-loader',
+		options: {
+			resources: [
+				path.resolve(__dirname, '../../src/style/mixin/border1px.scss'),
+				path.resolve(__dirname, '../../src/style/mixin/flex.scss'),
+				path.resolve(__dirname, '../../src/style/variables/color.scss'),
+				path.resolve(__dirname, '../../src/style/variables/size.scss'),
+			],
+		},
+	},
+];
 
 module.exports = [
-    {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-            loaders: {
-                scss: {
-                    use: sassLoader
-                },
-                css: {
-                    use: ['css-loader', { loader: 'postcss-loader', options: { sourceMap: true } }],
-                    fallback: "style-loader"
-                }
-            }
-        }
-    },
-    {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: [resolve('node_modules')]
-    },
-    {
-        test: /\.js$/,
-        loader: 'eslint-loader',
-        enforce: "pre",
-        include: [path.resolve(__dirname, 'src')], // 指定检查的目录
-        options: { // 这里的配置项参数将会被传递到 eslint 的 CLIEngine 
-            formatter: require('eslint-friendly-formatter') // 指定错误报告的格式规范
-        }
-    },
-    {
-        test: /\.scss$/,
-        loader: sassLoader
-    }
-]
+	{
+		test: /\.vue$/,
+		loader: 'vue-loader',
+		options: {
+			loaders: {
+				scss: {
+					use: sassLoader,
+				},
+				css: {
+					use: ['css-loader', { loader: 'postcss-loader', options: { sourceMap: true } }],
+					fallback: 'style-loader',
+				},
+			},
+		},
+	},
+	{
+		test: /\.js$/,
+		loader: 'babel-loader',
+		exclude: [resolve('node_modules')],
+	},
+	{
+		test: /\.js$/,
+		loader: 'eslint-loader',
+		enforce: 'pre',
+		include: [path.resolve(__dirname, 'src')], // 指定检查的目录
+		options: {
+			// 这里的配置项参数将会被传递到 eslint 的 CLIEngine
+			formatter: require('eslint-friendly-formatter'), // 指定错误报告的格式规范
+		},
+	},
+	{
+		test: /\.scss$/,
+		loader: sassLoader,
+	},
+	{
+		test: /\.css$/,
+		loader: sassLoader,
+	},
+];
--- example/example.index.js
+++ example/example.index.js
@@ -1,44 +1,44 @@
-import Vue from "vue"
-import App from "./App.vue"
-import VueRouter from "vue-router"
-import RUNUIX from "@runSrc/index.js"
-import routes from "./router.js"
-import './style/index.scss'
-import store from './vuex/index.js'
-import ExampleGroup from "./components/exampleGroup/ExampleGroup.vue";
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import RUNUIX from '../node_modules/aaa/lightUI.js';
+import '../library/lightUI.css';
+import { Button } from 'aaa';
+// import RUNUIX from '@runSrc/index.js';
+import routes from './router.js';
+import './style/index.scss';
+import store from './vuex/index.js';
+import ExampleGroup from './components/exampleGroup/ExampleGroup.vue';
 let router = new VueRouter({
-    routes
-})
+	routes,
+});
 Vue.component(ExampleGroup.name, ExampleGroup);
 
-[VueRouter].map((plugin) => {
-    Vue.use(plugin)
-})
-
+[VueRouter].map(plugin => {
+	Vue.use(plugin);
+});
+console.log(Button);
 Vue.use(RUNUIX, {
-    button: {
-        light: false
-    },
-    toast: {
-        duration: 100
-    },
-    dependence: {
-        store,
-        router
-    },
-    toast: {
-        duration: 5000
-    },
-})
-
-
+	button: {
+		light: false,
+	},
+	toast: {
+		duration: 100,
+	},
+	dependence: {
+		store,
+		router,
+	},
+	toast: {
+		duration: 5000,
+	},
+});
 
 new Vue({
-    el: "#app",
-    router,
-    store,
-    components: { App },
-    template: "<App/>",
-    created() {
-    }
-})
+	el: '#app',
+	router,
+	store,
+	components: { App },
+	template: '<App/>',
+	created() {},
+});
