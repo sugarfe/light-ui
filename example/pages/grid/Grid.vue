259d9f054153a71cfbe2fdc50531a481c38cf5d3
于鲸浩 <yujinghao@crc.com.hk>
2019-06-21 16:02:07 +0800

    优化grid组件demo

--- example/pages/grid/Grid.vue
+++ example/pages/grid/Grid.vue
@@ -2,58 +2,69 @@
   <l-page-content>
     <l-navbar slot="header">Grid</l-navbar>
     <example-group contentClass="group-content">
+      col
+      <template v-slot:content>
+        <l-radio-group
+          v-model="option.col"
+          horizontal
+        >
+          <l-radio :value="1">1</l-radio>
+          <l-radio :value="2">2</l-radio>
+          <l-radio :value="3">3</l-radio>
+          <l-radio :value="4">4</l-radio>
+          <l-radio :value="6">6</l-radio>
+          <l-radio :value="10">10</l-radio>
+        </l-radio-group>
+      </template>
+    </example-group>
+    <example-group contentClass="group-content">
+      square
+      <template v-slot:content>
+        <l-checkbox-group
+          v-model="option.square"
+          right
+        >
+          <l-checkbox :value="1">square</l-checkbox>
+        </l-checkbox-group>
+      </template>
+    </example-group>
+    <example-group contentClass="group-content">
       col 4 square
       <template v-slot:content>
-        <div class="index-page">
-          <l-grid
-            ref="grid"
-            :col="4"
-            :square="true"
-            spacing="1px"
-            vertical="1px"
-          >
-            <l-grid-item
-              v-for="item in data"
-              :key="item"
-              style="background-color:#ffb403"
-            ></l-grid-item>
-          </l-grid>
-        </div>
+        <l-grid
+          ref="grid"
+          :col="option.col"
+          :square="!!option.square[0]"
+          spacing="1px"
+          vertical="1px"
+        >
+          <l-grid-item
+            v-for="item in data"
+            :key="item"
+            class="grid-item flex-box-center"
+          >{{item}}</l-grid-item>
+        </l-grid>
       </template>
     </example-group>
   </l-page-content>
 </template>
 <script>
+import { debuglog } from 'util'
 export default {
-  components: {},
   data() {
     return {
-      data: 10
+      data: 10,
+      option: {
+        col: 2,
+        square: [1]
+      }
     }
-  },
-  computed: {},
-  created: function() {
-    // setTimeout(() => {
-    //   this.data += 3;
-    //   this.$nextTick(() => {
-    //     this.$refs["grid"].refresh();
-    //   });
-    // }, 2000);
-  },
-  beforeMount: function() {},
-  mounted: function() {},
-  beforeDestroy: function() {},
-  destroyed: function() {},
-  methods: {},
-  watch: {},
-  directives: {}
+  }
 }
 </script>
-<style lang="scss">
-.index-page {
-  height: 100%;
-  overflow-y: auto;
-  overflow-x: hidden;
-  -webkit-overflow-scrolling: touch;
+<style lang="scss" >
+.grid-item {
+  background-color: #ffb403;
+  color: #fff;
 }
 </style>
